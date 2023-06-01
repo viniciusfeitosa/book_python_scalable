08a16f83a58c7b4e41264f3a48927bbaf54473a9
Vinicius Pacheco <vfpweb@gmail.com>
2023-06-01 00:48:39 -0500

    Update file names

--- chapter5/conftest.py
+++ chapter5/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, clear_mappers
 
-from orm import mapper_registry, load_mappers
+from schema import mapper_registry, load_mappers
 
 
 @pytest.fixture
